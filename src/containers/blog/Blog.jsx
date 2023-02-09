672dc39e9a2b139f2dd8183667f385138fa1e47a
YitayalT <anex518@gmail.com>
2023-02-09 10:29:33 +0300

    adding the possibilty section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import './blog.css'
+import './blog.css';
+import { Article } from '../../components';
+import {} from './import'
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container__groupA">
+           <Article />
+        </div>
+
+        <div className="gpt3__blog-container__groupB"></div>
+           <Article />
+           <Article />
+           <Article />
+           <Article />
+      </div>
+    </div>
   )
 }
 
